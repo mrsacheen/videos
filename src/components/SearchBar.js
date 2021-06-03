c01fad24112e7e37291c05c26b9b011e3fb05112
Sachin Khanal <sackhanaal@gmail.com>
2021-06-03 15:36:51 -0500

    Search button implemented

---
+++ src/components/SearchBar.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+    state = {
+        term:''
+    }
+  onInputChange = (event) => {
+    this.setState({term: event.target.value})
+    
+  }
+  onFormSubmit = (event) =>{
+    event.preventDefault();
+    console.log(this.state)
+
+    // TODO: Make sure we call
+    //callback from parent component
+  }
+  
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit= {this.onFormSubmit} className="ui form">
+          <div className="ui fluid icon input">
+            <input 
+                type="text" 
+                placeholder="Video Search" 
+                value={this.state.term}
+                onChange={this.onInputChange}
+             />
+            <i className="search icon"></i>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
