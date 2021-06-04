1fa41eb37f24991437ad506cc11dbd85bd4ac42a
Sachin Khanal <sackhanaal@gmail.com>
2021-06-04 11:38:27 -0500

    Search button implemented

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
   }
   onFormSubmit = (event) =>{
     event.preventDefault();
-    console.log(this.state)
+    this.props.onFormSubmit(this.state.term)
 
     // TODO: Make sure we call
     //callback from parent component
